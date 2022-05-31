f2fef96df065092504e7f6dce6b4e70a665cf147
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-31 13:45:32 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
-  namespace :v1, defaults: { format: 'json' } do
-    get 'things', to:'things#index'
+  namespace :v1, defaults: { format: :json } do
+    get 'greetings', to: 'greetings#index'
   end
   get '*page', to:'static#index',constraints: ->(req) do
     !req.xhr? && req.format.html?
