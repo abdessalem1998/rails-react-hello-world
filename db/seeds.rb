d01669f2a3dce3b20a358aeeff54f19522d4df2e
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-31 13:36:20 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "message 1")
+Greeting.create(message: "message 2")
+Greeting.create(message: "message 3")
+Greeting.create(message: "message 4")
+Greeting.create(message: "message 5")
